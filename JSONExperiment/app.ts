0d46889295a7751e6d122c20f1cc895e7b713368
高垣 雄介 <yousukeman3@gmail.com>
2017-09-16 14:45:32 +0900

    a

--- JSONExperiment/app.ts
+++ JSONExperiment/app.ts
@@ -1,6 +1,6 @@
 ﻿"use strict";
 
-let myRequest = fetch("json.json");
+let myRequest = fetch("https://raw.githubusercontent.com/yousukeman3/JSONExperiment/master/JSONExperiment/json.json");
 myRequest.then(response => {
     return response.json();
 }).then((json) => {
