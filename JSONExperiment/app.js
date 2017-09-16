d6c01831f1d6ea8ea288b75b43772302b5b61674
yousukeman3 <yousukeman3@gmail.com>
2017-09-17 01:56:21 +0900

    browser-sync

--- JSONExperiment/app.js
+++ JSONExperiment/app.js
@@ -1,5 +1,5 @@
 "use strict";
-let myRequest = fetch("https://raw.githubusercontent.com/yousukeman3/JSONExperiment/master/JSONExperiment/json.json");
+let myRequest = fetch("json.json");
 myRequest.then(response => {
     return response.json();
 }).then((json) => {
